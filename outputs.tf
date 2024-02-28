f91951b5ef5dfbbd33f7d439d32a119ed0f3aa24
Mariusz Słowiński <mario.slowinski@gmail.com>
2024-02-28 15:15:26 +0100

    Add "object_id" to outputs.

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,9 @@ output "group" {
   value       = local.group
   sensitive   = false
 }
+
+output "object_id" {
+  description = "The object ID of the group."
+  value       = local.group.id
+  sensitive   = false
+}
